4f14f8d06350c13adf4a336e04a374fc7616b84f
dipankar-datta <dattapro5@gmail.com>
2019-06-11 19:42:55 +0530

    Implemented Lombok for entities and logger.

--- src/main/java/guru/springframework/services/impl/RecipeServiceImpl.java
+++ src/main/java/guru/springframework/services/impl/RecipeServiceImpl.java
@@ -3,11 +3,13 @@ package guru.springframework.services.impl;
 import guru.springframework.model.Recipe;
 import guru.springframework.repositories.RecipeRepository;
 import guru.springframework.services.RecipeService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.HashSet;
 import java.util.Set;
 
+@Slf4j
 @Service
 public class RecipeServiceImpl implements RecipeService {
 
@@ -19,6 +21,7 @@ public class RecipeServiceImpl implements RecipeService {
 
     @Override
     public Set<Recipe> getRecipes() {
+        log.debug("Inside get recipes");
         Set<Recipe> recipeSet = new HashSet<Recipe>();
 
         recipeRepository.findAll().iterator().forEachRemaining(recipeSet::add);
