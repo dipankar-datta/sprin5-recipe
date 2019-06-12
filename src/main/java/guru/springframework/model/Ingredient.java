1a7fc04f1df7dfc125d1606210bdb2386f64aa30
dipankar-datta <dattapro5@gmail.com>
2019-06-12 20:01:13 +0530

    Applied bootstrap css.

--- src/main/java/guru/springframework/model/Ingredient.java
+++ src/main/java/guru/springframework/model/Ingredient.java
@@ -1,13 +1,17 @@
 package guru.springframework.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 import java.math.BigDecimal;
 
 @Entity
 @EqualsAndHashCode(exclude = "recipe")
+@NoArgsConstructor
+@AllArgsConstructor
 @Data
 public class Ingredient {
 
